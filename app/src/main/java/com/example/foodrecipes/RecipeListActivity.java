e76775921d5a6bb29ed53f0c2c6c7eb8cf8c5019
cuncis <karang.reksa@gdc.co.id>
2019-08-26 08:52:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -0,0 +1,25 @@
+package com.example.foodrecipes;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RecipeListActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_recipe);
+
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (progressBar.getVisibility() == View.VISIBLE) {
+                    showProgressBar(false);
+                } else {
+                    showProgressBar(true);
+                }
+            }
+        });
+    }
+
+}
